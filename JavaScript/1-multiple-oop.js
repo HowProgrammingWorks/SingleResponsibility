51f4444b7ee8335f787db994bde8cacc24a2a793
tshemsedinov <timur.shemsedinov@gmail.com>
2019-07-10 16:10:03 +0300

    Improve example

--- JavaScript/1-multiple-oop.js
+++ JavaScript/1-multiple-oop.js
@@ -2,6 +2,9 @@
 
 const fs = require('fs').promises;
 
+const CTRL_C = 3;
+const ENTER = 13;
+
 class User {
   constructor(id, data) {
     this.id = id;
@@ -17,12 +20,45 @@ class User {
   async save() {
     const { id, data } = this;
     const json = JSON.stringify(data);
-    fs.writeFile(`./user${id}.json`, json);
+    await fs.writeFile(`./user${id}.json`, json);
   }
 
   toString() {
     const { name, login, email } = this.data;
     return `User: ${login} (${name}) <${email}>`;
+  }
+
+  static isPasswordValid(password) {
+    return password.length >= 7;
+  }
+
+  async changePassword() {
+    process.stdin.setRawMode(true);
+    process.stdout.write('Enter new password: ');
+    let resolve = null;
+    const input = [];
+    const done = () => {
+      process.stdin.removeAllListeners('data');
+      process.stdin.setRawMode(false);
+      resolve();
+    };
+    process.stdin.on('data', chunk => {
+      process.stdout.write('*');
+      const key = chunk[0];
+      if (key === CTRL_C) done();
+      if (key === ENTER) {
+        process.stdout.write('\n');
+        const password = Buffer.concat(input).toString();
+        const valid = User.isPasswordValid(password);
+        if (valid) this.data.password = password;
+        console.log('Password:', valid ? 'is valid' : 'is not valid');
+        done();
+      }
+      input.push(chunk);
+    });
+    return new Promise(r => {
+      resolve = r;
+    });
   }
 }
 
@@ -31,5 +67,7 @@ class User {
 (async () => {
   const user = await User.read(2073);
   console.log(`${user}`);
-  user.save();
+  await user.changePassword();
+  await user.save();
+  process.exit(0);
 })();
